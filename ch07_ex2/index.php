7f774a8579db6c483a73ac1b10643b86e20556c6
binarygalwalkin <coding@coding.net>
2019-03-07 03:11:01 +0000

    update by Coding WebIDE , https://ide.coding.net

--- ch07_ex2/index.php
+++ ch07_ex2/index.php
@@ -21,24 +21,32 @@
 
         <div id="data">
             <label>Investment Amount:</label>
-            <input type="text" name="investment"
-                   value="<?php echo $investment; ?>"/><br>
+             <select name="investment"> <?php for ($v = 10000; $v <= 50000; $v += 5000) : ?>
+               <option value="<?php echo $v; ?>"><?php echo $v; ?></option>
+            <?php endfor; ?>
+            </select><br>
+
 
             <label>Yearly Interest Rate:</label>
-            <input type="text" name="interest_rate"
-                   value="<?php echo $interest_rate; ?>"/><br>
+            <select name="interest_rate"><?php for ($v = 4; $v <= 12; $v += .5) : ?>
+            <option value="<?php echo $v; ?>" ><?php echo $v; ?></option>
+            <?php endfor; ?>           
+            </select><br>
 
+            
             <label>Number of Years:</label>
             <input type="text" name="years"
                    value="<?php echo $years; ?>"/><br>
+               
         </div>
-
+        
         <div id="buttons">
-            <label>&nbsp;</label>
-            <input type="submit" value="Calculate"/><br>
-        </div>
-
-    </form>
+            
+        <label>&nbsp;</label>
+            
+        <input type="submit" value="Calculate"/><br>
+      
+      </form>
     </main>
 </body>
 </html>
